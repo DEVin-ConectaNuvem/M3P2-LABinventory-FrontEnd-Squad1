62c0c31089968793b39abe91c1845c291d9b1595
edumartinsrib <edumartinsrib@live.com>
2022-05-16 20:55:48 -0300

    [INIT] DEVInventary

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <div>
+   
+
+    <div class="">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+
+  </div>
+
+  <RouterView />
+</template>
+
+<style>
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state: {
+  
+  },
+  
+  getters: {},
+
+  mutations: {
+   
+  },
+  actions: {
+  }
+});
+
+export default store;
