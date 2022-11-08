883319c43ac0d98b5a22cd1de8f3776bf93eccee
Eduardo Martins Ribeiro <edumartinsrib@live.com>
2022-11-07 23:17:21 -0300

    feat: adicionado jsonserver para views colaboradores

--- src/store/modules/collaborator/mutations.js
+++ src/store/modules/collaborator/mutations.js
@@ -1,24 +1,37 @@
-const UPDATE_COLLABORATOR_LOCAL_STORAGE = (state) => {
-  state.collaborators = localStorage.getItem("collaborators")
-    ? JSON.parse(localStorage.getItem("collaborators"))
-    : [];
+import { useAxios } from "../../../hooks";
+
+const UPDATE_COLLABORATOR_LOCAL_STORAGE = async (state) => {
+  try {
+    const res = (await useAxios().axios.get("http://localhost:3000/collaborators")) ||
+      [];
+    state.collaborators = res.data;
+    return res.data;
+  } catch (error) {
+    throw new Error("Erro ao obter atualizar colaboradores");
+  }
 };
 
 const SET_COLLABORATOR_LOCAL_STORAGE = (state) => {
   localStorage.setItem("collaborators", JSON.stringify(state.collaborators));
 }
 
-const REGISTER_COLLABORATOR = (state, payload) => {
-  state.collaborators.push(payload);
-  localStorage.setItem("collaborators", JSON.stringify(state.collaborators));
+const REGISTER_COLLABORATOR = async (state, payload) => {
+  try {
+    const res = await useAxios().axios.post("http://localhost:3000/collaborators", payload);
+    state.collaborators.push(res.data);
+    return res.data;
+  } catch (error) {
+    console.log(error.message)
+  }
 };
 
-const EDIT_COLLABORATOR = (state, payload) => {
-  state.collaborators.forEach((collaborator, index) => {
-    if (collaborator.id === payload.id) {
-      state.collaborators[index] = payload;
-    }
-  });
+const EDIT_COLLABORATOR = async (state, payload) => {
+  try {
+    const res = await useAxios().axios.put(`http://localhost:3000/collaborators/${payload.id}`, payload);
+    return res.data;
+  } catch (error) {
+    console.log(error.message)
+  }
 };
 
 export {
