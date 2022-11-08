fd32ea70ce53bd96aca0f1ed73b9b19c17267e96
Eduardo Martins Ribeiro <edumartinsrib@live.com>
2022-11-08 18:24:35 -0300

    fix: removido console.logs e functions desnecessárias

--- src/store/modules/collaborator/mutations.js
+++ src/store/modules/collaborator/mutations.js
@@ -11,17 +11,13 @@ const UPDATE_COLLABORATOR_LOCAL_STORAGE = async (state) => {
   }
 };
 
-const SET_COLLABORATOR_LOCAL_STORAGE = (state) => {
-  localStorage.setItem("collaborators", JSON.stringify(state.collaborators));
-}
-
 const REGISTER_COLLABORATOR = async (state, payload) => {
   try {
     const res = await useAxios().axios.post("http://localhost:3000/collaborators", payload);
     state.collaborators.push(res.data);
     return res.data;
   } catch (error) {
-    console.log(error.message)
+    throw new Error("Erro ao registrar colaborador");
   }
 };
 
@@ -30,13 +26,12 @@ const EDIT_COLLABORATOR = async (state, payload) => {
     const res = await useAxios().axios.put(`http://localhost:3000/collaborators/${payload.id}`, payload);
     return res.data;
   } catch (error) {
-    console.log(error.message)
+    throw new Error("Erro ao editar colaborador");
   }
 };
 
 export {
   UPDATE_COLLABORATOR_LOCAL_STORAGE,
   REGISTER_COLLABORATOR,
-  EDIT_COLLABORATOR,
-  SET_COLLABORATOR_LOCAL_STORAGE
+  EDIT_COLLABORATOR
 };
