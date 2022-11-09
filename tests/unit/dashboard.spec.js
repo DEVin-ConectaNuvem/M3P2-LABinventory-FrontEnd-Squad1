b52fe1abed1d65f2f401608c64abbf345e5953ef
Luiz Gustavo Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-11-09 00:04:19 -0300

    feat: configurando testes

--- tests/unit/dashboard.spec.js
+++ tests/unit/dashboard.spec.js
@@ -1,10 +1,23 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallow, createLocalVue, mount } from "@vue/test-utils";
 import DashboardView from "../../src/views/Dashboard/DashboardView.vue";
+import { createStore } from "vuex";
+import authModule from '../../src/store/modules/auth';
+import collaboratorModule from '../../src/store/modules/collaborator';
+import itemsModule from '../../src/store/modules/item';
+import configModule from '../../src/store/modules/config';
+
 
 describe("DashboardView.vue", () => {
+    let mockedIncFn
+    let store
+    beforeEach(() => {
+        mockedIncFn = jest.fn()
+        store = createStore({
+          });
+    })
     it("Caso nenhum item cadastrado retorna mensagem para cadastrar.", () => {
-      const wrapper = shallowMount(DashboardView, {
-        propsData: { itens: [] },
+      const wrapper = mount(DashboardView, {
+        global: { plugins: [store] },
       });
   
       expect(wrapper.text()).toMatch(
