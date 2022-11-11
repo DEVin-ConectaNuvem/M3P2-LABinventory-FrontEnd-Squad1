0c2c15b063f500678bbfbba30dd4135db75d4526
Luiz Gustavo Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-11-10 22:16:48 -0300

    feat: cards 19-20-21-22-27-28-29-30-32-33

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -4,7 +4,7 @@ describe('Dashboard', () => {
 
         cy.wait(1000)
         
-        cy.get('input[placeholder="Digite o e-mail"]').type("edumartinsrib@live.com");
+        cy.get('input[placeholder="Digite o e-mail"]').type("teste@teste.com.br");
         cy.get('input[placeholder="Digite sua senha"]').type("12345678");
 
         cy.get('button:contains("Acessar")').first().click();
@@ -13,13 +13,76 @@ describe('Dashboard', () => {
 
         cy.get('input[placeholder*="Buscar item..."]').type("99999999999999999");
 
+        cy.wait(1000)
+
         cy.contains("Ainda não há itens cadastrados com este termo de pesquisa");
 
-        cy.get('a').contains('Realizar novo cadastro').click();
+        cy.get('a:contains("Realizar novo cadastro")').first().click();
 
         cy.wait(1000)
         
         cy.contains("Criação e edição de itens");
 
+    }),
+    it('Visita a pagina dashboard e valida se os cards com informações totais de colaboradores está sendo exibida e ao clicar no card, abre um modal que nós redireciona para listagem de colaboradores', () => {
+        cy.visit('http://localhost:3000');
+
+        cy.wait(1000)
+        
+        cy.get('input[placeholder="Digite o e-mail"]').type("teste@teste.com.br");
+        cy.get('input[placeholder="Digite sua senha"]').type("12345678");
+
+        cy.get('button:contains("Acessar")').first().click();
+
+        cy.wait(3000);
+
+        cy.get('h4[class="my-1"]');
+
+        cy.get('p:contains("Colaboradores")').click();
+
+        cy.contains("Gostaria de ver mais informações sobre colaboradores ?")
+
+        cy.get('button:contains("Verificar!")').click();
+
+        cy.contains("Listagem de colaboradores")
+
+    }),
+    it('Visita a pagina dashboard e valida se os cards com informações totais de itens, valores e empréstimos está sendo exibida e ao clicar no card, abre um modal que nós redireciona para listagem de itens', () => {
+        cy.visit('http://localhost:3000');
+
+        cy.wait(1000)
+        
+        cy.get('input[placeholder="Digite o e-mail"]').type("teste@teste.com.br");
+        cy.get('input[placeholder="Digite sua senha"]').type("12345678");
+
+        cy.get('button:contains("Acessar")').first().click();
+
+        cy.wait(3000);
+
+        cy.get('h4[class="my-1"]');
+
+        cy.get('p:contains("Itens")').click();
+        cy.contains("Gostaria de ver mais informações sobre itens ?");
+        cy.get('button:contains("Verificar!")').click();
+        cy.contains("Listagem de itens");
+
+        cy.get('i[class="fa-solid fa-bars fs-2 text-white"]').click();
+        cy.get('span:contains("Inventário")').click();
+
+        cy.get('p:contains("Valores")').click();
+        cy.contains("Gostaria de ver mais informações sobre itens ?");
+        cy.get('button:contains("Verificar!")').click();
+        cy.contains("Listagem de itens");
+        
+        cy.get('span:contains("Inventário")').click();
+
+        cy.get('p:contains("Empréstimos")').click();
+        cy.contains("Gostaria de ver mais informações sobre empréstimos ?");
+        cy.get('button:contains("Verificar!")').click();
+        cy.contains("Listagem de itens");
+
+        cy.get('span:contains("Inventário")').click();
+
+        cy.contains("Dashboard");
     })
   })
