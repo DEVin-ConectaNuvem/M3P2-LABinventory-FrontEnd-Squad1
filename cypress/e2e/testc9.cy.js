327d234b382723b8a2d1a82ff84c27b6e4db673e
Breno-MT <hentgenp@gmail.com>
2022-11-11 20:01:40 -0300

    refact: retirado testes antigos e colocados no outro arquivo

--- cypress/e2e/testc9.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Teste de clickar no botao Esqueceu a senha', () => {
-    it('Retornar Função em desenvolvimento!', () => {
-        cy.visit('http://localhost:3000')
-        cy.contains("Esqueceu a senha?").click()
-        cy.contains("Função em desenvolvimento!")
-        cy.get("[data-testid=login-google-button]").click()
-        cy.contains("Função em desenvolvimento!")
-    })
-})
