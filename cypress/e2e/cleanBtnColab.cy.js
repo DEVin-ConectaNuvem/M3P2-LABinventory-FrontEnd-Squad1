36165ab4d9713008a72c9eca6d03da1779ca6e7a
Rafael Telles Carneiro <rafatellescarneiro@gmail.com>
2022-11-13 19:01:01 -0300

    feat: verificando botão limpar

---
+++ cypress/e2e/cleanBtnColab.cy.js
@@ -0,0 +1,29 @@
+describe('Teste do Botão Limpar no cadastro de Contribuinte',()=>{
+    it('Testar se o botão "limpar" está realmente limpando todos os campos', ()=>{
+        cy.login('test@test.com.br', '12345678')
+        
+        cy.wait(3000)
+
+        cy.visit('http://localhost:3000/#/colaboradores')
+
+        cy.wait(1000)
+
+        cy.get("[data-testid=colab-name]").type("teste12")
+        cy.get("[data-testid=colab-gender]").select("Masculino")
+        cy.get("[data-testid=colab-date]").type("1990-04-04")
+        cy.get("[data-testid=colab-phone]").type("(61) 99999-9999")
+        cy.get("[data-testid=colab-email]").type("teste@teste.com")
+        cy.get("[data-testid=colab-position]").select("Desenvolvedor Backend")
+        cy.get("[data-testid=colab-zipcode]").type("72130007").trigger('keydown', {
+            key: 'Enter',
+          });
+        cy.get("[data-testid=colab-neighborhood]").type("testestesteste") 
+        cy.get("[data-testid=colab-street]").type("teste teste tststststs") 
+        cy.get("[data-testid=colab-number]").type("5") 
+        cy.get("[data-testid=colab-complement]").type("abcde") 
+        cy.get("[data-testid=colab-reference]").type("fghij") 
+
+        cy.get("[data-testid=reset-button]").click();
+        cy.contains('Edição cancelada!')
+    })
+})
