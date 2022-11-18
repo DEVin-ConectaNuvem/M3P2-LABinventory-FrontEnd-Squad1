b80b54113dcda34d45bf2e2c0ee91a061477af77
Breno-MT <hentgenp@gmail.com>
2022-11-17 22:22:47 -0300

    refact: melhorado lógica de testes

--- cypress/e2e/registerItems.cy.js
+++ cypress/e2e/registerItems.cy.js
@@ -9,7 +9,7 @@ describe("Realiza os testes de registro de itens", () => {
         cy.request("GET", "/items").as("getItems").then((interception) => {
 			items = interception.body;
 		});
-	})
+	});
 
 	it("Edita um item com sucesso", () => {
 		cy.contains("Headset Gamer Teste").click();
@@ -22,7 +22,7 @@ describe("Realiza os testes de registro de itens", () => {
         cy.get("[data-testid=itemView-input-model]").clear().type("CH1N4INH0US3");
         cy.get("[data-testid=itemView-input-description]").clear().type("Este produto foi criado na China.");
         cy.contains("Editar").click();
-	})
+	});
     
     it("Testa botão de cancelar", () => {
         cy.contains("Produto Caro").click();
@@ -37,19 +37,17 @@ describe("Realiza os testes de registro de itens", () => {
         cy.contains("Cancelar").click();
         cy.contains("Edição cancelada!")
         cy.url().should("eq", "http://localhost:3000/#/")
-	})
+	});
     
-    it('Cadastro realizado com sucesso!', () => {
-        cy.openSidebar();
-        cy.get('a[href="#/itens"]').click();
-        cy.get("[data-testid=itemView-input-title]").type("Produto Criado");
-        cy.get("[data-testid=itemView-input-category]").focus().select("Outros");
-        cy.get("[data-testid=itemView-input-value]").type("600.00");
-        cy.get("[data-testid=itemView-input-brand]").type("Made in Teste");
-        cy.get("[data-testid=itemView-input-model]").type("T3ST3INH0US3");
-        cy.get("[data-testid=itemView-input-description]").type("Este produto foi teste na House.");
-        cy.get("[data-testid='button-reg']").click();
+    it('Cadastro realizado com sucesso! Com upload de imagem!', () => {
+        cy.registerItem("Cadeira Nova", "1620.00", "AMD", "M0D3LN3W", "Descrição da melhor Cadeira.", true)
         cy.contains("Cadastro de colaborador realizado com sucesso!");
         cy.get("a[href='#/']").click();
-    })
+    });
+
+    it('Cadastro realizado com sucesso! Sem upload de imagem!', () => {
+        cy.registerItem("Cadeira Nova", "1620.00", "AMD", "M0D3LN3W", "Descrição da melhor Cadeira.", false)
+        cy.contains("Cadastro de colaborador realizado com sucesso!");
+        cy.get("a[href='#/']").click();
+    });
 })
